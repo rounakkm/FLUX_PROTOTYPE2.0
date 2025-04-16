3cf48a18150411ad2883cf63d4a810d69c7307eb
rounakkm <mrounak2006@gmail.com>
2025-04-16 15:44:10 +0530

    removed t1

--- front/t1.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const logoutBtn = document.getElementById("logoutBtn");
-
-    logoutBtn.addEventListener("click", () => {
-        if (confirm("Are you sure you want to logout?")) {
-            window.location.href = "login.html";
-        }
-    });
-});
